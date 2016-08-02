0c906f50c262045a21890171fdf2fe20be8136f3
James Wright <booff42@bigpond.com>
2016-08-02 22:44:49 +1000

    Removed comments + debugging
    Same as the last but with the old stuff as comments and the debugging
    removed.

--- rong/game/paddle.py
+++ rong/game/paddle.py
@@ -40,15 +40,9 @@ class Paddle:
     def position(self, new_position):
         new_position = copy.deepcopy(new_position)
 
-        #top_left_vertex = new_position + self.__DIAGONAL.inverted
-        #top_right_vertex = new_position + self.__DIAGONAL.inverted_y
-        #bottom_right_vertex = new_poisition + self.__DIAGONAL
-        #bottom_left_vertex = new_position + self.__DIAGONAL.inverted_x
-
         points = self.__new_points(new_position)
 
         if new_position != self.position:
-            i = 0
             for point in points:
                 for boundary in self._boundaries:
                     if not utilities.point_is_on_this_side_of_interval(
@@ -63,58 +57,6 @@ class Paddle:
                         )
                         new_position += correction
                         points = self.__new_points(new_position)
-                i += 1
-
-        '''
-        top_right_vertex = new_position + self.SIZE.x_component
-        bottom_left_vertex = new_position + self.SIZE.y_component
-        bottom_right_vertex = new_position + self.SIZE
-
-        if new_position.y < 0:
-            new_position.y = 0
-            self.position = new_position
-            return
-
-        if bottom_left_vertex.y > self._canvas_size.y:
-            new_position.y -= bottom_left_vertex.y - self._canvas_size.y
-            self.position = new_position
-            return
-
-        if self._in_right_half:
-            if game_variables.free_movement_enabled.get():
-                if top_right_vertex.x > self._right_boundary:
-                    new_position.x -= (
-                        top_right_vertex.x - self._right_boundary
-                    )
-                    self.position = new_position
-                    return
-
-                if new_position.x < self._middle_right_boundary:
-                    new_position.x += (
-                        self._middle_right_boundary - new_position.x
-                    )
-                    self.position = new_position
-                    return
-            elif new_position.x != self._right_boundary:
-                return
-        elif game_variables.free_movement_enabled.get():
-            if new_position.x < self.__BOUNDARY_PADDING:
-                new_position.x = self.__BOUNDARY_PADDING
-                self.position = new_position
-                return
-
-            if new_position.x > self._middle_left_boundary:
-                new_position.x = self._middle_left_boundary
-                self.position = new_position
-                return
-        elif new_position.x != self.__BOUNDARY_PADDING:
-                return
-
-        self._position = new_position
-        self._top_right_vertex = top_right_vertex
-        self._bottom_right_vertex = bottom_right_vertex
-        self._bottom_left_vertex = bottom_left_vertex
-        '''
 
         self._position = new_position
         self._top_left_vertex = points[0]
@@ -157,32 +99,6 @@ class Paddle:
             canvas.winfo_width(),
             canvas.winfo_height()
         )
-
-        '''
-        halfway_point = self._canvas_size.x / 2
-        self._right_boundary = (
-            self._canvas_size.x \
-            - self.__BOUNDARY_PADDING \
-            - self.SIZE.x
-        )
-        self._middle_left_boundary = (
-            halfway_point - self.__BOUNDARY_PADDING - self.SIZE.x
-        )
-        self._middle_right_boundary = halfway_point + self.__BOUNDARY_PADDING
-
-        x_offset = self.__BOUNDARY_PADDING
-
-        if in_right_half:
-            x_offset = self._right_boundary
-            self._keys = self.__RIGHT_HALF_KEYS
-        else:
-            self._keys = self.__LEFT_HALF_KEYS
-
-        self.position = utilities.Vector(
-            x_offset,
-            (self._canvas_size.y - self.SIZE.y) / 2
-        )
-        '''
 
         x_offset = self.__BOUNDARY_PADDING + self.SIZE.x / 2
         if in_right_half:
